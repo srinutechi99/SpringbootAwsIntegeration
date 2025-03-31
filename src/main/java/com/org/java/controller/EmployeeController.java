879f307c1496ade36edbe423a5fb00d9e420eda8
srinutechi99 <srinivasarao.javasoft@gmail.com>
2025-03-31 10:12:46 +0530

    ETTETE

--- src/main/java/com/org/java/controller/EmployeeController.java
+++ src/main/java/com/org/java/controller/EmployeeController.java
@@ -41,7 +41,7 @@ public class EmployeeController {
 	public String welcomeTest() {
 		logger.trace("it is trace method welcome calling");
 		logger.info("wel come method is working");
-		return "WELCOME AWS EKS APPLICATION DEVELOPMENT AWS INTEGRATION"; 
+		return "WELCOME AWS EKS APPLICATION DEVELOPMENT AWS INTEGRATION NEW"; 
 
 	}
 
